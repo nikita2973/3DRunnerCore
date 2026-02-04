ed906a64a29d29e1fc3516e9d346a6922bafcee4
Nikita <52929330+nikita2973@users.noreply.github.com>
2026-02-04 17:56:17 +0200

    Update Animation

--- Assets/Scripts/Noise/MovingNoise.cs
+++ Assets/Scripts/Noise/MovingNoise.cs
@@ -1,6 +1,14 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class MovingNoise : MonoBehaviour
 {
-  
+  [SerializeField]private float _duration;
+  [SerializeField]private float _targetLane;
+
+  private void OnEnable()
+  {
+    transform.DOMoveZ(transform.position.z-700, _duration);
+  }
 }
